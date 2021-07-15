969b9a685a6b4246e794fb1a7e4ffdf4cd489b0c
marco.ardizzone99 <marco.ardizzone99@gmail.com>
2021-07-15 18:46:53 +0200

    Added menu, music and playerprefs

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager: MonoBehaviour
+{
+    public AudioSource MyAudioSource;
+    float musicVolume;
+
+    private static AudioManager _instance;
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+    void Start()
+    {
+        MyAudioSource = GetComponent<AudioSource>();
+        musicVolume = PlayerPrefs.GetFloat("MVolume");
+        MyAudioSource.Play();
+    }
+
+    private void Update()
+    {
+        MyAudioSource.volume = musicVolume;
+    }
+
+    public void SetVolume(float volume)
+    {
+        musicVolume = volume;
+        PlayerPrefs.SetFloat("MVolume", 1f);
+    }
+}
---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public Slider volSlider;
+    public Dropdown qualityDropdown;
+    public Dropdown resolutionDropdown;
+    public Toggle fullscreenToggle;
+    private int screenInt;
+    Resolution[] resolutions;
+    private bool isFullScreen = false;
+
+    const string prefName = "optionvalue";
+    const string resName = "resolutionoption";
+
+    private void Awake()
+    {
+        screenInt = PlayerPrefs.GetInt("togglestate");
+        if (screenInt == 1)
+        {
+            isFullScreen = true;
+            fullscreenToggle.isOn = true;
+        }
+        else
+            fullscreenToggle.isOn = false;
+
+        //aggiungo i listener agli sliders
+        resolutionDropdown.onValueChanged.AddListener(new UnityAction<int>(index => { 
+            PlayerPrefs.SetInt(resName, resolutionDropdown.value);
+            PlayerPrefs.Save();
+        }));
+
+        qualityDropdown.onValueChanged.AddListener(new UnityAction<int>(index =>
+        {
+            PlayerPrefs.SetInt(prefName, qualityDropdown.value);
+            PlayerPrefs.Save();
+        }));
+    }
+
+    void Start()
+    {
+        volSlider.value = PlayerPrefs.GetFloat("MVolume", 1f);
+        //volMixer.SetFloat("volume", PlayerPrefs.GetFloat("MVolume"));
+
+        qualityDropdown.value = PlayerPrefs.GetInt(prefName, 1);
+
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for(int i=0; i< resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height + " " + resolutions[i].refreshRate + "Hz";
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height &&
+                resolutions[i].refreshRate == Screen.currentResolution.refreshRate)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = PlayerPrefs.GetInt(resName, currentResolutionIndex);
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    /*public void changeVol(float volume)
+    {
+        PlayerPrefs.SetFloat("MVolume", volume);
+        volMixer.SetFloat("volume", PlayerPrefs.GetFloat("MVolume"));
+    }*/
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+        if (isFullScreen == false)
+            PlayerPrefs.SetInt("togglestate", 0);
+        else
+        {
+            isFullScreen = true;
+            PlayerPrefs.SetInt("togglestate", 1);
+        }
+    }
+}
